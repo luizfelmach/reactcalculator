dd85774917583377828585e5364aa62a07155c58
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2022-01-22 15:05:36 -0300

    Add initial structure

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import "./Calculator.css";
+
+class Calculator extends Component {
+  render() {
+    return <div className="calculator"></div>;
+  }
+}
+
+export { Calculator };
